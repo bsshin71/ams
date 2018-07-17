815722aed3d32b9478f4881b57f5b72fad1db73c
bsshin71 <beumsu@gmail.com>
2018-07-17 23:31:12 +0900

    update telegraf and query file

--- telegraf_altibase/altibase.go
+++ telegraf_altibase/altibase.go
@@ -150,7 +150,7 @@ func (m *Altibase) runSQL(acc telegraf.Accumulator, db *sql.DB) error {
 				}
 
 				key := v[element.PivotKey].(string)
-				data := v[element.Fields[0]]
+				data := v[element.Fields[1]]
 				fields[key] = data
 			}
 			acc.AddFields(element.SeriesName, fields, tags)
